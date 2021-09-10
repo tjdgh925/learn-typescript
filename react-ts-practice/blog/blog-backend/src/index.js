86296ade63ed9271952af5d7e5ab70bc2598085c
장성호 <tjdgh925@gmail.com>
2021-09-10 21:22:28 +0900

    백엔드 적용하기

---
+++ react-ts-practice/blog/blog-backend/src/index.js
@@ -0,0 +1,51 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+});
+
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+// app.use(async (ctx, next) => {
+//   console.log(ctx.url);
+//   console.log(1);
+//   if (ctx.query.authorized !== '1') {
+//     ctx.status = 401; //Unauthorized
+//     return;
+//   }
+//   await next();
+//   // next().then(() => {
+//   console.log('END');
+//   // });
+// });
+
+// app.use((ctx, next) => {
+//   console.log(2);
+//   next();
+// });
+
+// app.use((ctx) => {
+//   ctx.body = 'hello world';
+// });
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
