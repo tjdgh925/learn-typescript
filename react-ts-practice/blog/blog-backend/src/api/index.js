7d92f9a6b236cd0af7f009860f14ffddb2758bdf
장성호 <tjdgh925@gmail.com>
2021-09-11 21:27:39 +0900

    백엔드 적용하기 (mongoDB)

---
+++ react-ts-practice/blog/blog-backend/src/api/index.js
@@ -0,0 +1,12 @@
+import Router from 'koa-router';
+import posts from './posts';
+
+const api = new Router();
+
+// api.get('/test', (ctx) => {
+//   ctx.body = 'test 성공';
+// });
+
+api.use('/posts', posts.routes());
+
+export default api;
--- react-ts-practice/blog/blog-backend/src/index.js
+++ react-ts-practice/blog/blog-backend/src/index.js
@@ -1,51 +1,83 @@
-const Koa = require('koa');
-const Router = require('koa-router');
+/* eslint-disable no-global-assign */
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
 
-const app = new Koa();
-const router = new Router();
+// require('dotenv').config();
+// const Koa = require('koa');
+// const Router = require('koa-router');
+// const bodyParser = require('koa-bodyparser');
+// const mongoose = require('mongoose');
 
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
+// const api = require('./api');
 
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
+// const { PORT, MONGO_URI } = process.env;
 
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-});
+// mongoose
+//   .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//   });
 
-router.get('/about', (ctx) => {
-  ctx.body = '소개';
-});
+// const app = new Koa();
+// const router = new Router();
 
-app.use(router.routes()).use(router.allowedMethods());
+// router.use('/api', api.routes());
 
-// app.use(async (ctx, next) => {
-//   console.log(ctx.url);
-//   console.log(1);
-//   if (ctx.query.authorized !== '1') {
-//     ctx.status = 401; //Unauthorized
-//     return;
-//   }
-//   await next();
-//   // next().then(() => {
-//   console.log('END');
-//   // });
+// app.use(bodyParser());
+
+// app.use(router.routes()).use(router.allowedMethods());
+
+// const port = PORT || 4000;
+
+// app.listen(port, () => {
+//   console.log('Listening to port %d', port);
 // });
 
-// app.use((ctx, next) => {
-//   console.log(2);
-//   next();
-// });
+// // router.get('/', (ctx) => {
+// //   ctx.body = '홈';
+// // });
 
-// app.use((ctx) => {
-//   ctx.body = 'hello world';
-// });
+// // router.get('/about/:name?', (ctx) => {
+// //   const { name } = ctx.params;
+// //   ctx.body = name ? `${name}의 소개` : '소개';
+// // });
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
+// // router.get('/posts', (ctx) => {
+// //   const { id } = ctx.query;
+// //   ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+// // });
+
+// // router.get('/about', (ctx) => {
+// //   ctx.body = '소개';
+// // });
+
+// // app.use(router.routes()).use(router.allowedMethods());
+
+// // app.use(async (ctx, next) => {
+// //   console.log(ctx.url);
+// //   console.log(1);
+// //   if (ctx.query.authorized !== '1') {
+// //     ctx.status = 401; //Unauthorized
+// //     return;
+// //   }
+// //   await next();
+// //   // next().then(() => {
+// //   console.log('END');
+// //   // });
+// // });
+
+// // app.use((ctx, next) => {
+// //   console.log(2);
+// //   next();
+// // });
+
+// // app.use((ctx) => {
+// //   ctx.body = 'hello world';
+// // });
+
+// // app.listen(4000, () => {
+// //   console.log('Listening to port 4000');
+// // });
