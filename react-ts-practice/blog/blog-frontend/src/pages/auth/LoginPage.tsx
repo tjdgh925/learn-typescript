40424057b9f46249fc24c3bd190103eb6eea4e56
장성호 <tjdgh925@gmail.com>
2021-09-16 23:22:41 +0900

    Update LoginPage.tsx

--- react-ts-practice/blog/blog-frontend/src/pages/auth/LoginPage.tsx
+++ react-ts-practice/blog/blog-frontend/src/pages/auth/LoginPage.tsx
@@ -12,6 +12,7 @@ import PersonIcon from '@material-ui/icons/Person';
 import LockIcon from '@material-ui/icons/Lock';
 
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 interface LoginPageProps {}
 
@@ -21,29 +22,43 @@ const useStyles = makeStyles((theme: Theme) =>
       display: 'flex',
       flexDirection: 'column',
       flexWrap: 'wrap',
-      background: '#c2c2c2',
       textAlign: 'center',
-      width: '100vw',
-      height: '100vh',
+      justifyContent: 'center',
+      minWidth: '750px',
+      width: '50%',
+      height: '80vh',
       margin: `${theme.spacing(0)} auto`,
     },
     card: {
       marginTop: theme.spacing(10),
       marginLeft: theme.spacing(10),
       marginRight: theme.spacing(10),
+      border: 'none',
+      boxShadow: 'none',
     },
     title: {
       color: 'blue',
+      marginBottom: theme.spacing(8),
     },
-    form: {
-      margin: theme.spacing(3),
-    },
+    form: {},
     authFindContainer: {
       display: 'flex',
-      flexWrap: 'wrap',
       justifyContent: 'space-between',
       alignItems: 'center',
-      marginTop: theme.spacing(3),
+      marginTop: theme.spacing(2),
+    },
+    button: {
+      color: 'white',
+      height: 50,
+      fontSize: 25,
+    },
+    buttonLogin: {
+      marginTop: theme.spacing(2),
+      backgroundColor: '#95519B',
+      marginBottom: theme.spacing(1),
+    },
+    buttonSignUp: {
+      backgroundColor: '#6E6B6F',
     },
   })
 );
@@ -56,48 +71,55 @@ const LoginPage = () => {
         <Typography variant="h2" className={classes.title}>
           CO-CO-BOB
         </Typography>
-        <form
-          className={classes.form}
-          id="login"
-          placeholder="Email"
-          autoComplete="off"
-        >
-          <TextField
+        <Container className={classes.form}>
+          <form id="login" placeholder="Email" autoComplete="off">
+            <TextField
+              fullWidth
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PersonIcon />
+                  </InputAdornment>
+                ),
+              }}
+              type="email"
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+              type="password"
+            />
+          </form>
+          <Container className={classes.authFindContainer} maxWidth="xl">
+            <FormControlLabel control={<Checkbox />} label="아이디 저장" />
+            <ButtonGroup variant="text">
+              <Button> 아이디 찾기 </Button>
+              <Button> 비밀번호 찾기 </Button>
+            </ButtonGroup>
+          </Container>
+          <Button
+            type="submit"
+            form="login"
+            className={clsx(classes.button, classes.buttonLogin)}
             fullWidth
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PersonIcon />
-                </InputAdornment>
-              ),
-            }}
-            type="email"
-          />
-          <TextField
-            fullWidth
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockIcon />
-                </InputAdornment>
-              ),
-            }}
-            type="password"
-          />
-        </form>
-        <Container className={classes.authFindContainer}>
-          <FormControlLabel control={<Checkbox />} label="아이디 저장" />
-          <ButtonGroup variant="text">
-            <Button>아이디 찾기</Button>
-            <Button>비밀번호 찾기</Button>
-          </ButtonGroup>
-        </Container>
-        <ButtonGroup fullWidth={true} orientation="vertical">
-          <Button type="submit" form="login">
+          >
             로그인
           </Button>
-          <Button fullWidth={true}>회원가입</Button>
-        </ButtonGroup>
+          <Button
+            fullWidth
+            className={clsx(classes.button, classes.buttonSignUp)}
+          >
+            회원가입
+          </Button>
+        </Container>
       </Card>
     </Container>
   );
